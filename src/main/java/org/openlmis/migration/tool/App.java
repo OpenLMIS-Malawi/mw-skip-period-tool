f067fb9a6970be7179e69c805df347df623902b7
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-03-09 09:27:23 +0100

    Added standard maven plugins

--- src/main/java/org/openlmis/migration/tool/App.java
+++ src/main/java/org/openlmis/migration/tool/App.java
@@ -2,8 +2,8 @@ package org.openlmis.migration.tool;
 
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
 
 }
