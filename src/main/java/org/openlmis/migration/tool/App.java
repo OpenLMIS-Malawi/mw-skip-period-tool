d7cdbbaf94a0401fd7a71302c6638f992e089a37
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-03-10 08:24:20 +0100

    Added spring boot

--- src/main/java/org/openlmis/migration/tool/App.java
+++ src/main/java/org/openlmis/migration/tool/App.java
@@ -5,10 +5,15 @@ import com.beust.jcommander.ParameterException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Optional;
 
-public class App {
+@SpringBootApplication
+public class App implements CommandLineRunner {
   private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
 
   /**
@@ -17,6 +22,14 @@ public class App {
    * @param args a list of arguments
    */
   public static void main(String[] args) {
+    SpringApplication application = new SpringApplication(App.class);
+    application.setBannerMode(Banner.Mode.OFF);
+
+    application.run(args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
     Optional<Arguments> parsed = parse(args);
     parsed.ifPresent(arguments -> System.out.println(arguments.getFile()));
   }
