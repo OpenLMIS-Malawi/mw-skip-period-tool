340d7e3798ac0df486b54b5552119c5c40dab18a
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-04-05 11:03:54 +0200

    Fixed problem with parallel streams

--- src/main/java/mw/gov/health/lmis/migration/tool/scm/ScmDatabaseHandler.java
+++ src/main/java/mw/gov/health/lmis/migration/tool/scm/ScmDatabaseHandler.java
@@ -6,34 +6,32 @@ import com.healthmarketscience.jackcess.DatabaseBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import lombok.Getter;
 import mw.gov.health.lmis.migration.tool.config.ToolProperties;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.TimeZone;
 
-@Getter
 @Component
 public class ScmDatabaseHandler {
-  private final Database database;
 
-  /**
-   * Create a new instance of this handler.
-   */
   @Autowired
-  public ScmDatabaseHandler(ToolProperties properties) {
-    File accessFile = properties.getConfiguration().getAccessFile();
+  private ToolProperties toolProperties;
+
+  public Database getDatabase() {
+    File accessFile = toolProperties.getConfiguration().getAccessFile();
+
+    String timeZoneId = toolProperties.getParameters().getTimeZone();
+    TimeZone timeZone = TimeZone.getTimeZone(timeZoneId);
 
     try {
-      database = new DatabaseBuilder()
+      return new DatabaseBuilder()
           .setFile(accessFile)
           .setReadOnly(true)
-          .setTimeZone(TimeZone.getTimeZone(properties.getParameters().getTimeZone()))
+          .setTimeZone(timeZone)
           .open();
     } catch (IOException exp) {
       throw new IllegalStateException("Can't open database: " + accessFile, exp);
     }
   }
-
 }
