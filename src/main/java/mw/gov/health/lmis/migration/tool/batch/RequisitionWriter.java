fb4f82c02f1e326f62c6b992be9ecf6f771958c3
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-06-21 13:32:23 +0200

    MW-324: Add duplicate check before save into database

--- src/main/java/mw/gov/health/lmis/migration/tool/batch/RequisitionWriter.java
+++ src/main/java/mw/gov/health/lmis/migration/tool/batch/RequisitionWriter.java
@@ -1,11 +1,14 @@
 package mw.gov.health.lmis.migration.tool.batch;
 
+import com.google.common.collect.Sets;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import lombok.EqualsAndHashCode;
 import mw.gov.health.lmis.migration.tool.config.ToolProperties;
 import mw.gov.health.lmis.migration.tool.openlmis.ExternalStatus;
 import mw.gov.health.lmis.migration.tool.openlmis.fulfillment.domain.Order;
@@ -14,12 +17,16 @@ import mw.gov.health.lmis.migration.tool.openlmis.fulfillment.domain.OrderStatus
 import mw.gov.health.lmis.migration.tool.openlmis.fulfillment.domain.ProofOfDelivery;
 import mw.gov.health.lmis.migration.tool.openlmis.fulfillment.repository.OrderRepository;
 import mw.gov.health.lmis.migration.tool.openlmis.fulfillment.repository.ProofOfDeliveryRepository;
+import mw.gov.health.lmis.migration.tool.openlmis.referencedata.domain.Facility;
+import mw.gov.health.lmis.migration.tool.openlmis.referencedata.domain.ProcessingPeriod;
 import mw.gov.health.lmis.migration.tool.openlmis.referencedata.domain.Program;
+import mw.gov.health.lmis.migration.tool.openlmis.referencedata.repository.FacilityRepository;
 import mw.gov.health.lmis.migration.tool.openlmis.requisition.domain.Requisition;
 import mw.gov.health.lmis.migration.tool.openlmis.requisition.repository.RequisitionRepository;
 
-import java.util.Collection;
 import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 
 @Component
 public class RequisitionWriter implements ItemWriter<List<Requisition>> {
@@ -35,35 +42,48 @@ public class RequisitionWriter implements ItemWriter<List<Requisition>> {
   private ProofOfDeliveryRepository proofOfDeliveryRepository;
 
   @Autowired
+  private FacilityRepository facilityRepository;
+
+  @Autowired
   private ToolProperties toolProperties;
 
   @Autowired
   private AppBatchContext context;
 
+  private Set<Signature> signatures = Sets.newConcurrentHashSet();
+
   /**
    * Writes Requisitions into OpenLMIS database.
    */
   @Override
-  public void write(List<? extends List<Requisition>> items) throws Exception {
-    items
-        .stream()
-        .flatMap(Collection::stream)
-        .forEach(requisition -> {
+  public synchronized void write(List<? extends List<Requisition>> items) throws Exception {
+    for (int i = 0, length = items.size(); i < length; ++i) {
+      List<Requisition> list = items.get(i);
+
+      for (int j = 0, size = list.size(); j < size; ++j) {
+        Requisition requisition = list.get(j);
+        Program program = context.findProgramById(requisition.getProgramId());
+
+        if (signatures.add(new Signature(requisition))) {
           requisitionRepository.save(requisition);
 
           if (requisition.getStatus() == ExternalStatus.RELEASED) {
-            createOrder(requisition);
+            createOrder(requisition, program);
           }
-        });
+        } else {
+          Facility facility = facilityRepository.findOne(requisition.getFacilityId());
+          ProcessingPeriod period = context.findPeriodById(requisition.getProcessingPeriodId());
+
+          LOGGER.warn(
+              "Requisition for facility {}, program {} and period {} exists. Skipping...",
+              facility.getCode(), program.getCode(), period.getName()
+          );
+        }
+      }
+    }
   }
 
-  private void createOrder(Requisition requisition) {
-    Program program = context.getPrograms()
-        .stream()
-        .filter(elem -> requisition.getProgramId().equals(elem.getId()))
-        .findFirst()
-        .orElse(null);
-
+  private void createOrder(Requisition requisition, Program program) {
     if (null == program) {
       LOGGER.error("Can't find program with id {}", requisition.getProgramId());
     }
@@ -79,6 +99,19 @@ public class RequisitionWriter implements ItemWriter<List<Requisition>> {
     order = orderRepository.save(order);
 
     proofOfDeliveryRepository.save(new ProofOfDelivery(order));
+  }
+
+  @EqualsAndHashCode
+  private static final class Signature {
+    private final UUID facility;
+    private final UUID program;
+    private final UUID period;
+
+    Signature(Requisition requisition) {
+      this.facility = requisition.getFacilityId();
+      this.program = requisition.getProgramId();
+      this.period = requisition.getProcessingPeriodId();
+    }
   }
 
 }
