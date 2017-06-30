c85f8359e6c4103ab842b3f91c85544d91e02c63
Łukasz Lewczyński <llewczynski@soldevelo.com>
2017-07-01 00:02:09 +0200

    Removed unused repositories

--- src/main/java/mw/gov/health/lmis/migration/tool/openlmis/referencedata/repository/FacilityTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package mw.gov.health.lmis.migration.tool.openlmis.referencedata.repository;
-
-import mw.gov.health.lmis.migration.tool.openlmis.referencedata.domain.FacilityType;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.UUID;
-
-public interface FacilityTypeRepository extends CrudRepository<FacilityType, UUID> {
-}
